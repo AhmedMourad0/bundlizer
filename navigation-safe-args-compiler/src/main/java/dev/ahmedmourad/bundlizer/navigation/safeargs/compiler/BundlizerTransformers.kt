edae57bbf044c9125bf9fb2dde7e2dd1f83a12f0
Ahmed Mourad <dev.ahmedmourad73744@gmail.com>
2020-05-10 06:19:57 +0200

    Added initial draft for `toBundle` plugin, an issue arisen that we can't get the data types of properties in this compiler phase, work is being done at Arrow Meta to fix this.

--- navigation-safe-args-compiler/src/main/java/dev/ahmedmourad/bundlizer/navigation/safeargs/compiler/BundlizerTransformers.kt
+++ navigation-safe-args-compiler/src/main/java/dev/ahmedmourad/bundlizer/navigation/safeargs/compiler/BundlizerTransformers.kt
@@ -4,40 +4,113 @@ import arrow.meta.CliPlugin
 import arrow.meta.Meta
 import arrow.meta.invoke
 import arrow.meta.quotes.Transform
-import arrow.meta.quotes.namedFunction
+import arrow.meta.quotes.classBody
 import org.jetbrains.kotlin.psi.KtClass
+import org.jetbrains.kotlin.psi.KtProperty
 import org.jetbrains.kotlin.psi.psiUtil.containingClass
 
-const val FQ_NAME_NAV_ARGS = "androidx.navigation.NavArgs"
-const val NAME_KOTLINX_SERIALIZABLE = "kotlinx.serialization.Serializable"
+private const val FQ_NAME_NAV_ARGS = "androidx.navigation.NavArgs"
+private const val KOTLINX_SERIALIZABLE = "kotlinx.serialization.Serializable"
 
-val Meta.toBundleTransformer: CliPlugin
+private val PRIMITIVES_AND_PRIMITIVES_ARRAYS = arrayOf(
+    "Int",
+    "IntArray",
+    "Float",
+    "FloatArray",
+    "Double",
+    "DoubleArray",
+    "Long",
+    "LongArray",
+    "String",
+    "StringArray"
+)
+
+internal val Meta.toBundle: CliPlugin
     get() = "toBundleTransformer" {
-        meta(namedFunction({
-            name == "toBundle" && this.containingClass()?.hasSuperType(FQ_NAME_NAV_ARGS) == true
-        }) { c ->
+        meta(classBody({
+            this.containingClass()?.hasSuperType(FQ_NAME_NAV_ARGS) == true
+        }) { _ ->
+            val functionToTransform = this.functions.value.first { it.name == "toBundle" }
+            val properties = this.properties.value.map(KtProperty::toSimpleProperty)
+            val rootBundleName = "result"
             Transform.replace(
-                replacing = c,
+                replacing = functionToTransform,
                 newDeclaration = """
-                    fun toBundle(): Bundle {
-                    |   val result = Bundle()
-                    |   if (Parcelable::class.java.isAssignableFrom(Bundle::class.java)) {
-                    |       result.putParcelable("child", this.child as Parcelable?)
-                    |   } else if (Serializable::class.java.isAssignableFrom(Bundle::class.java)) {
-                    |       result.putSerializable("child", this.child as Serializable?)
-                    |   } else if (Bundle::class.java.annotations.any { it.annotationClass.qualifiedName == "$NAME_KOTLINX_SERIALIZABLE" }) {
-                    |           result.putBundle("child", this.child.bundle(Child.serializer()))
-                    |   } else {
-                    |       throw UnsupportedOperationException(Bundle::class.java.name +
-                    |           " must implement Parcelable or Serializable or must be an Enum.")
-                    |   }
-                    |   return result
-                    |}
-                """.function
+                   |fun toBundle(): Bundle {
+                   |    val $rootBundleName = Bundle()
+                   |    ${properties.generateBundleConstructionBody(rootBundleName)}
+                   |    return $rootBundleName
+                   |}
+                """.trimIndent().function
             )
         })
     }
 
+private fun List<SimpleProperty>.generateBundleConstructionBody(rootBundleName: String): String {
+    return this.joinToString("\n\n") { property ->
+        when {
+
+            property.type in PRIMITIVES_AND_PRIMITIVES_ARRAYS -> property.generatePrimitiveOrPrimitiveArrayBody(
+                rootBundleName
+            )
+
+            property.type.startsWith("Array<") -> TODO()
+
+            else -> property.generateCustomDataTypeBody(rootBundleName)
+        }
+    }
+}
+
+private fun SimpleProperty.generatePrimitiveOrPrimitiveArrayBody(rootBundleName: String): String {
+    return """
+       |$rootBundleName.put$type("$name", this.$name)
+    """.trimIndent()
+}
+
+private fun SimpleProperty.generateCustomDataTypeBody(rootBundleName: String): String {
+    val typeNullability = typeNullability(isNullable)
+    val serializerNullability = serializerNullability(isNullable)
+    return """
+       |if (Parcelable::class.java.isAssignableFrom($type::class.java)) {
+       |    $rootBundleName.putParcelable("$name", this.$name as Parcelable$typeNullability)
+       |} else if (Serializable::class.java.isAssignableFrom(Bundle::class.java)) {
+       |    $rootBundleName.putSerializable("$name", this.$name as Serializable$typeNullability)
+       |} else if ($type::class.java.annotations.any { it.annotationClass.qualifiedName == "$KOTLINX_SERIALIZABLE" }) {
+       |    $rootBundleName.putBundle("$name", this.$name.bundle($type.serializer()$serializerNullability))
+       |} else {
+       |    throw UnsupportedOperationException($type::class.java.name +
+       |        " must implement Parcelable or Serializable, be annotated with"
+       |        " $KOTLINX_SERIALIZABLE or must be an Enum."
+       |    )
+       |}
+    """.trimIndent()
+}
+
+private fun typeNullability(isNullable: Boolean): String {
+    return if (isNullable) "?" else ""
+}
+
+private fun serializerNullability(isNullable: Boolean): String {
+    return if (isNullable) ".nullable" else ""
+}
+
 private fun KtClass.hasSuperType(fqName: String): Boolean {
-    return this.superTypeListEntries.any { it.name == FQ_NAME_NAV_ARGS }
+    return this.superTypeListEntries.any { it.name == fqName }
+}
+
+private data class SimpleProperty(
+    val name: String,
+    val type: String,
+    val isNullable: Boolean
+)
+
+private fun KtProperty.toSimpleProperty(): SimpleProperty {
+    val typeInSource = this.typeReference!!.text
+    val isNullable = typeInSource.endsWith("?")
+    val typeFqName = if (isNullable) {
+        typeInSource.dropLast(1)
+    } else {
+        typeInSource
+    }
+    return SimpleProperty(this.name!!, typeFqName, isNullable)
 }
